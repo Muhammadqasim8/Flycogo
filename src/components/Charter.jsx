1c85b1900bbb73c28c3d67f91517735342c9997b
Muhammad Qasim <qasimramzan78@gmail.com>
2024-08-29 23:46:10 +0500

    fix path

--- src/components/Charter.jsx
+++ src/components/Charter.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './fly.css'
+import './Fly.css'
 
 import { Images } from '../utils/Images';
 
