adc0c06b36b315db472ff998e4e407f01311e069
Muhammad Qasim <qasimramzan78@gmail.com>
2024-08-30 17:08:59 +0500

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   };
 
   return (
-    <div className='container mx-auto flex justify-between py-8  px-4 xl:px-[42px]'>
+    <div className='container mx-auto flex justify-between py-8 px-4 xl:px-[42px]'>
       <div className='max-h-[73px] max-w-[88px]'>
         <img src={logo} alt="Logo" />
       </div>
@@ -39,29 +39,27 @@ const Header = () => {
 
       {/* Sidebar Menu */}
       {isSidebarOpen && (
-        <div className='fixed inset-0 bg-black bg-opacity-50 z-50'>
-          <div className='fixed top-0 left-0 w-[330px] h-[90%] bg-white z-50 shadow-lg p-4' style={{ height: '' }}>
-            {/* Close Button */}
-            <div className='flex justify-end'>
-              <img src={Images.cros} alt="Close Sidebar" onClick={toggleSidebar} className="cursor-pointer mb-4" />
-            </div>
-
-            {/* Buttons */}
-            <div className='flex gap-5 px-4 mb-4'>
-              <Button className="bg-white flex-1 text-black border-2 border-[#2E3072]">Log in</Button>
-              <Button className="bg-[#2E3072] flex-1 text-white border-2 border-[#2E3072]">Register</Button>
-            </div>
-
-            <ul className='flex flex-col text-[#464646] gap-4'>
-              <a href="#"><li className='font-medium text-base'>Home</li></a>
-              <a href="#"><li className='font-medium text-base'>About</li></a>
-              <a href="#"><li className='font-medium text-base'>Empty Leg Listing</li></a>
-              <a href="#"><li className='font-medium text-base'>Operators</li></a>
-              <a href="#"><li className='font-medium text-base'>Flights</li></a>
-              <a href="#"><li className='font-medium text-base'>Support</li></a>
-              <a href="#"><li className='font-medium text-base'>Contact Us</li></a>
-            </ul>
+        <div className='absolute top-0 right-0 w-[320px] h-full bg-white shadow-lg z-50 p-4 transition-transform transform'>
+          {/* Close Button */}
+          <div className='flex justify-end'>
+            <img src={Images.cros} alt="Close Sidebar" onClick={toggleSidebar} className="cursor-pointer mb-4" />
           </div>
+
+          {/* Buttons */}
+          <div className='flex gap-5 px-4 mb-4'>
+            <Button className="bg-white flex-1 text-black border-2 border-[#2E3072]">Log in</Button>
+            <Button className="bg-[#2E3072] flex-1 text-white border-2 border-[#2E3072]">Register</Button>
+          </div>
+
+          <ul className='flex flex-col text-[#464646] gap-4'>
+            <a href="#"><li className='font-medium text-base'>Home</li></a>
+            <a href="#"><li className='font-medium text-base'>About</li></a>
+            <a href="#"><li className='font-medium text-base'>Empty Leg Listing</li></a>
+            <a href="#"><li className='font-medium text-base'>Operators</li></a>
+            <a href="#"><li className='font-medium text-base'>Flights</li></a>
+            <a href="#"><li className='font-medium text-base'>Support</li></a>
+            <a href="#"><li className='font-medium text-base'>Contact Us</li></a>
+          </ul>
         </div>
       )}
     </div>
